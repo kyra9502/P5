e7a18570813ce2960f945f5bc9fdec1371d79f39
kyra9502 <kyradoc9502@hotmail.com>
2018-07-12 15:29:06 +0200

    little change

--- view/index.php
+++ view/index.php
@@ -1,10 +1,10 @@
 <?php
 
 session_start();
-require('../controller/controller.php');
-include('../view/header.php');
+//require('../controller/controller.php');
+//include('../view/header.php');
 
-$post = listPost();
+//$post = listPost();
 ?>
 
 <!-- Header -->
@@ -12,11 +12,13 @@ $post = listPost();
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
-                <img class="img-responsive" src="../img/moi2.png" alt="">
+                
                 <div class="intro-text">
                     <span class="name">Développeur PHP / Symfony</span>
+                    <img class="img-responsive" src="../img/moi2.png" alt="">
                     <hr class="star-primary">
                     <span class="skills">La développeuse qu'il vous faut !</span>
+
                 </div>
             </div>
         </div>
