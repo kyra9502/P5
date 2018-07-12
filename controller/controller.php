9df072eb072d3d5c7e377ce4baf94ef8773a7798
kyra9502 <kyradoc9502@hotmail.com>
2018-07-12 17:03:16 +0200

    correction in controller.php for OrderPost

--- controller/controller.php
+++ controller/controller.php
@@ -4,7 +4,7 @@ use P5\Model as database;
 function listPost()
 {
 	$postManager = new database\PostManager();
-	$post = $postManager->getPost();
+	$post = $postManager->getOrderPost();
 
 	return $post;
 }
