c72f63c8de8a5b3f4a0a985ec27e983ab1d12b03
kyra9502 <kyradoc9502@hotmail.com>
2018-07-27 16:57:28 +0200

    begin part admin

---
+++ admin/view/header.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title></title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Theme CSS -->
+    <link href="../css/freelancer.css" rel="stylesheet">
+    <link href="../css/freelancer.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+</head>
+
+<body id="page-top" class="index">
+
+    <!-- Navigation -->
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top navbar-custom">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand">Doctrinal Pauline</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="../../view/index.php">Retour accueil</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="">Dashboard</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="">Articles</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="">Commentaires</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="">Déconnexion</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- jQuery -->
+    <script src="../vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="../js/jqBootstrapValidation.js"></script>
+    <script src="../js/contact_me.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="../js/freelancer.min.js"></script>
+</body>
---
+++ admin/view/login.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+
+include('../view/header.php');
+?>
+
+<section id="portfolio">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center"></br></br>
+                    <h2>Connexion</h2>
+                    <hr class="star-primary">
+                </div>
+                <div class="row">
+	                <div class="img-responsive text-center portfolio-item">
+	                    <a href="login.php#se_connecter">
+	                        <img src="../img/connexion.jpg"  alt="">
+	                        </a>
+	                </div>
+                </div>
+            </div>
+ </section>
+
+<section id="se_connecter"class="success">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <form method="post" action="connexion.php">
+                	<p>Adresse email : <input type="text" name="email"/></p></br>
+                	<p>Mot de passe : <input type="password" name="m2p"/></p></br>
+                	<input type="submit" name="valider" value="Se conncter"/>
+				</form>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+<?php
+include("../view/footer.php");
+?>
